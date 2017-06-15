e2532162d78775a66850004d739c34f8692188c9
wangzhenxin@reganfund.com <wangzhenxin@reganfund.com>
2017-06-15 17:44:30 +0800

    iii

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FatherComponent } from './father-child/father.component';
+import { ChildComponent } from './father-child/child.component';
 import {RouterModule,Routes} from '@angular/router';;
 const Routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent},
+  { path: 'father-child',  component: FatherComponent},
   { path: 'about',  loadChildren: () => new Promise(resolve => {
     (require as any).ensure([],(require: any) => {
       resolve(require('./about/about.module').AboutModule);
@@ -19,7 +22,9 @@ const Routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FatherComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
